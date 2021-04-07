a37e7d0254e3297afb1bbbeacb02ca64684c910c
Philip Swiezak <philip.swiezak@ticos-systems.ch>
2021-04-07 23:26:57 +0200

    Layout fixes

--- js/index.js
+++ js/index.js
@@ -2,24 +2,23 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 1 || document.documentElement.scrollTop > 1) {
-    // document.getElementById("navbar").style.top = "0";
+  if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+    // On scroll
     document.getElementById("flex-container").style.backgroundColor="rgba(255, 255, 255, 1)";
     document.getElementById("flex-container").setAttribute("style","-webkit-filter:invert(100)");
     document.getElementById("flex-container").style.height="46px";
-
-
+    // document.getElementById("flex-container").style.transition="0.7s"
 
     document.getElementById("kasia").style.border="0px";
 
 
   } else {
+    // Normal state:
+
     document.getElementById("flex-container").style.height="70px";
     document.getElementById("flex-container").setAttribute("style","-webkit-filter:invert(0)");
 
     document.getElementById("kasia").style.border="1px solid black";
-
-
 
   }
 }
